5452adcc959ece5be26e3da235b4b7e3ce2cab99
Justin Nicholas <nicholasj964@yahoo.com>
2022-08-06 11:44:27 -0500

    get request and display items working.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,37 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('JQ Working');
+    $('#add-item-button').on('click', addItem);
+    getList();
 }
+
+function getList(){
+    console.log('in getList');
+    $.ajax({
+        method: 'GET',
+        url: '/items'
+    }).then( function(response) {
+        console.log(response);
+        displayItems(response);
+    }).catch( function(err) {
+        console.log(err);
+    })
+}
+
+function displayItems(itemList) {
+    console.log('in display');
+    for ( let item of itemList ){
+        $('.item-list').append(`
+        <tr data-id=${item.id}>
+            <td>${item.item}</td>
+            <td><button id="complete-button">Complete</button></td>
+            <td><button id="delete-button">Delete</button></td>
+        </tr>`)
+    }
+}
+
+function addItem() {
+    let newItem = $('#new-item').val();
+
+}
+
